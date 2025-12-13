cd8ccf429c53feb23e6439735db266647e370c19
Pouya Elhami <108701997+pouya204@users.noreply.github.com>
2025-12-13 13:08:21 +0330

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,15 @@
+import math
+import random
 
+def euclidean(a, b):
+    return math.hypot(a[0] - b[0], a[1] - b[1])
+
+def roulette_wheel(weights):
+    s = sum(weights)
+    r = random.uniform(0, s)
+    cumulative = 0
+    for i, w in enumerate(weights):
+        cumulative += w
+        if cumulative >= r:
+            return i
+    return len(weights) - 1
